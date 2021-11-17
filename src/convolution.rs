4342b64009ba9f77db73c3b68f98fa3999f76f26
Matt J <bordenjardine@gmail.com>
2021-11-16 21:26:19 -0800

    good enough

--- src/convolution.rs
+++ src/convolution.rs
@@ -1,32 +1,8 @@
-use std::collections::VecDeque;
-
-pub fn convolve(
-  input_sample: i16,
-  impulse_response: &[i16],
-  history_buffer: &mut VecDeque<i128>
-  ) -> i16 {
-  let inp = input_sample as i128;
-  for (i, kernal_sample) in impulse_response.iter().enumerate() {
-    match history_buffer.get_mut(i) {
-      Some(history_sample) => *history_sample += inp * (*kernal_sample as i128),
-      None => return 0
-    }
-  }
-
-  match history_buffer.pop_front() {
-    Some(output) => {
-      history_buffer.push_back(0);
-      return output as i16;
-    },
-    None => return 0
-  }
-}
-
 pub fn convolve_signals(
   input_signal: &[i16],
   impulse_response: &[i16],
-  ) -> Vec<i128> {
-  let mut output_buffer: Vec<i128> = Vec::new();
+) -> Vec<i16> {
+  let mut output_buffer: Vec<i16> = Vec::new();
 
   // queue to keep track of convolutions so far
   // starts off with a bunch of 0s
@@ -35,15 +11,16 @@ pub fn convolve_signals(
     output_buffer.push(0);
   }
 
-  println!("total {}:", input_signal.len());
+  println!("total: {}", input_signal.len());
   //convolve each sample with IR
   for (i, input_sample) in input_signal.iter().enumerate() {
-    let inp = *input_sample as i128;
+    let s = input_sample / 64;
     for (j, kernal_sample) in impulse_response.iter().enumerate() {
-      output_buffer[i+j] += inp * (*kernal_sample as i128);
+      output_buffer[i+j] = output_buffer[i+j].saturating_add(s.saturating_mul(*kernal_sample / 1024));
     }
-    print!(".");
+    if i > 0 && 128 % i == 0 {
+      print!(".");
+    }
   }
-
   output_buffer
-  }
+}
